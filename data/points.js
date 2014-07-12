abd438879a4131a7bd7dc8f5774eabed9251bd26
Daniele Moraschi <daniele.moraschi@dxi-easycall.net>
2014-07-12 11:13:06 +0100

    updated point.js for all the new examples

--- data/points.js
+++ data/points.js
@@ -39,6 +39,40 @@ var LocsA = [
     }
 ];
 
+var LocsAv2 = [
+    {
+        lat: 45.9,
+        lon: 10.9,
+        title: 'Zone A1',
+        html: '<h3>Content A1</h3>',
+        type : 'circle',
+        circle_options: {
+            radius: 200000
+        },
+        draggable: true
+    },
+    {
+        lat: 44.8,
+        lon: 1.7,
+        title: 'Draggable',
+        html: '<h3>Content B1</h3>',
+        show_infowindow: false,
+        visible: true,
+        draggable: true
+    },
+    {
+        lat: 51.5,
+        lon: -1.1,
+        title: 'Title C1',
+        html: [
+            '<h3>Content C1</h3>',
+            '<p>Lorem Ipsum..</p>'
+        ].join(''),
+        zoom: 8,
+        visible: true
+    }
+];
+
 
 var LocsB = [
     {
@@ -84,6 +118,54 @@ var LocsB = [
 ];
 
 
+var LocsBv2 = [
+    {
+        lat: 52.1,
+        lon: 11.3,
+        title: 'Title A2',
+        html: [
+            '<h3>Content A2</h3>',
+            '<p>Lorem Ipsum..</p>'
+        ].join(''),
+        zoom: 8
+    },
+    {
+        lat: 51.2,
+        lon: 22.2,
+        title: 'Title B2',
+        html: [
+            '<h3>Content B2</h3>',
+            '<p>Lorem Ipsum..</p>'
+        ].join(''),
+        zoom: 8,
+        type : 'circle',
+        circle_options: {
+            radius: 100000
+        }
+    },
+    {
+        lat: 49.4,
+        lon: 35.9,
+        title: 'Title C2',
+        html: [
+            '<h3>Content C2</h3>',
+            '<p>Lorem Ipsum..</p>'
+        ].join(''),
+        zoom: 4
+    },
+    {
+        lat: 47.8,
+        lon: 15.6,
+        title: 'Title D2',
+        html: [
+            '<h3>Content D2</h3>',
+            '<p>Lorem Ipsum..</p>'
+        ].join(''),
+        zoom: 6
+    }
+];
+
+
 var LocsAB = LocsA.concat(LocsB);
 
 
@@ -91,7 +173,12 @@ var LocsC = [
     {
         lat: 45.4654,
         lon: 9.1866,
-        title: 'Milan, Italy'
+        title: 'Milan, Italy',
+        type : 'circle',
+        circle_options: {
+            radius: 1000
+        },
+        visible: false
     },
     {
         lat: 47.36854,
@@ -136,6 +223,84 @@ var LocsD = [
         lon: 11.57706,
         title: 'Munich, Germany',
         html: '<h3>Munich, Germany</h3>'
+    }
+];
+
+var Circles = [
+    {
+        lat: 51.51386,
+        lon: -0.09559,
+        title: 'Draggable marker',
+        circle_options: {
+            radius: 160
+        },
+        stroke_options: {
+            strokeColor: '#aaaa00',
+            fillColor: '#eeee00'
+        },
+        draggable: true
+    },
+    {
+        lat: 51.51420,
+        lon: -0.09303,
+        title: 'Draggable circle',
+        circle_options: {
+            radius: 50
+        },
+        stroke_options: {
+            strokeColor: '#aa0000',
+            fillColor: '#ee0000'
+        },
+        visible: false,
+        draggable: true
+    },
+    {
+        lat: 51.51498,
+        lon: -0.09097,
+        circle_options: {
+            radius: 80
+        },
+        visible: false,
+        draggable: true
+    },
+    {
+        lat: 51.51328,
+        lon: -0.09021,
+        circle_options: {
+            radius: 160,
+            editable: true
+        },
+        title: 'Editable circle',
+        html: 'Change my size',
+        visible: false,
+        draggable: true
+    },
+    {
+        lat: 51.51211,
+        lon: -0.09050,
+        circle_options: {
+            radius: 130
+        },
+        stroke_options: {
+            strokeColor: '#00aa00',
+            fillColor: '#00aa00'
+        },
+        visible: false
+    },
+    {
+        lat: 51.51226,
+        lon: -0.09435,
+        circle_options: {
+            radius: 100
+        },
+        draggable: true
+    },
+    {
+        lat: 51.513,
+        lon: -0.08410,
+        type: 'marker',
+        title: 'Simple marker',
+        html: 'I\'m a simple marker.'
     }
 ];
 
